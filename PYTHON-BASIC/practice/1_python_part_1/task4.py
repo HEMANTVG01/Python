4c2c5daa5cd56440613e079c6809452cb6a3052b
HEMANTVG01 <hhemantvg@griddynamics.com>
2025-03-24 18:03:42 +0530

    Update task4.py

--- PYTHON-BASIC/practice/1_python_part_1/task4.py
+++ PYTHON-BASIC/practice/1_python_part_1/task4.py
@@ -11,3 +11,21 @@ from typing import List
 
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
     ...
+
+def power_diff(arr):
+    n = len(arr)
+    r = []
+    for i in range(n):
+        if i == 0:
+            r.append(arr[i]**2)
+        else:
+            power = arr[i] ** 2
+            prev = arr[i-1] ** 2
+            diff = power - (prev - arr[i-1])
+            r.append(diff)
+    return r
+
+if __name__ == "__main__":
+    arr = [1,2,3]
+    r1 = power_diff(arr)
+    print(r1)
