b582bd9e2f36addd6be3b1df63f2b24ac81a5cee
HEMANTVG01 <hhemantvg@griddynamics.com>
2025-03-24 18:12:49 +0530

    Update task1.py

--- PYTHON-BASIC/practice/1_python_part_1/task1.py
+++ PYTHON-BASIC/practice/1_python_part_1/task1.py
@@ -16,3 +16,18 @@ from typing import List, Any
 
 def delete_from_list(list_to_clean: List, item_to_delete: Any) -> List:
     ...
+
+
+def delete_element(lst, element):
+    lst = [x for x in lst if x != element]
+    return lst
+
+if __name__ == "__main__":
+    lst = [1, 2, 3, 4, 3]
+    element = 3
+    r = delete_element(lst, element)
+    print(r)  
+
+print(delete_element(['a', 'b', 'c', 'b', 'd'], 'b'))  
+print(delete_element([1, 2, 3], 'b'))  
+print(delete_element([], 'b')) 
