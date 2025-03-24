233091e2ad31f26ab3d14bd532594d91998bd9f0
HEMANTVG01 <hhemantvg@griddynamics.com>
2025-03-24 18:19:57 +0530

    Update task2.py

--- PYTHON-BASIC/practice/1_python_part_1/task2.py
+++ PYTHON-BASIC/practice/1_python_part_1/task2.py
@@ -14,3 +14,21 @@ from typing import Dict
 
 def set_to_dict(dict_to_update: Dict[str, int], **items_to_set) -> Dict:
     ...
+
+
+def dict_val(d, **kwargs):
+    for key, value in kwargs.items():
+        if key in d:
+            if value > d[key]:
+                d[key] = value  
+        else:
+            d[key] = value  
+    return d
+
+if __name__ == "__main__":
+    d = {'a': 1, 'b': 2, 'c': 3}
+    r = dict_val(d, a=0, b=4)  
+    print(r)
+
+print(dict_val({}, a=0))  
+print(dict_val({'a': 5}))  
