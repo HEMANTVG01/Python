1f6ed7d3c239b05ec842b1b1de63c1f09e9fe1fd
HEMANTVG01 <hhemantvg@griddynamics.com>
2025-03-24 18:25:50 +0530

    Update task3.py

--- PYTHON-BASIC/practice/1_python_part_1/task3.py
+++ PYTHON-BASIC/practice/1_python_part_1/task3.py
@@ -17,3 +17,21 @@ from typing import Iterable
 
 def build_from_unique_words(*lines: Iterable[str], word_number: int) -> str:
     ...
+
+def build_from_unique_words(*lines, word_number):
+    result = []
+    for line in lines:
+        words = list(dict.fromkeys(line.split()))
+        if word_number < len(words):
+            result.append(words[word_number]) 
+    return ' '.join(result) 
+
+if __name__ == "__main__":
+    r1 = build_from_unique_words('a b c', '1 1 1 2 3', 'cat dog milk', word_number=1)
+    print(r1)
+    r2 = build_from_unique_words('a b c', '', 'cat dog milk', word_number=0)
+    print(r2)
+    r3 = build_from_unique_words('1 2', '1 2 3', word_number=10)
+    print(r3)
+    r4 = build_from_unique_words(word_number=10)
+    print(r4)
