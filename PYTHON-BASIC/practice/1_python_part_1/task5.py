ccca8335e7497cee81ed8e1df8fe12c57d49e630
HEMANTVG01 <hhemantvg@griddynamics.com>
2025-03-24 18:03:13 +0530

    Update task5.py

--- PYTHON-BASIC/practice/1_python_part_1/task5.py
+++ PYTHON-BASIC/practice/1_python_part_1/task5.py
@@ -14,3 +14,21 @@ Examples:
 
 def remove_duplicated_words(line: str) -> str:
     ...
+
+def remove_duplicated_words(a):
+    n = a.split()
+    u = []
+    for i in n:
+        if i not in u:
+            u.append(i)
+    return ' '.join(u)
+
+if __name__ == "__main__":
+    a = 'cat cat dog 1 dog 2'
+    r = remove_duplicated_words(a)
+    print(r)
+
+
+print(remove_duplicated_words('cat cat dog 1 dog 2'))
+print(remove_duplicated_words('cat cat cat'))
+print(remove_duplicated_words('1 2 3'))
