09df55099adb603837a2654e938dbd90cf4c4e5a
HEMANTVG01 <hhemantvg@griddynamics.com>
2025-04-01 12:32:31 +0530

    Update task_read_write_2.py

--- PYTHON-BASIC/practice/2_python_part_2/task_read_write_2.py
+++ PYTHON-BASIC/practice/2_python_part_2/task_read_write_2.py
@@ -22,3 +22,35 @@ def generate_words(n=20):
         words.append(word)
 
     return words
+
+############
+import random
+import string
+
+def generate_words(num_words=5, word_length=3):
+    words = []
+    for _ in range(num_words):
+        word = ''.join(random.choices(string.ascii_lowercase, k=word_length))
+        words.append(word)
+    return words
+
+def write_words_to_files(words):
+    with open('file1.txt', 'w', encoding='utf-8') as file1:
+        file1.write('\n'.join(words))
+    with open('file2.txt', 'w', encoding='cp1252') as file2:
+        file2.write(','.join(reversed(words)))
+
+words = generate_words()
+write_words_to_files(words)
+print("Files written: file1.txt (UTF-8), file2.txt (CP1252)")
+
+
+########
+#file1.txt = knv
+byb
+vqg
+dud
+hdd
+
+#file2.txt
+hdd,dud,vqg,byb,knv
