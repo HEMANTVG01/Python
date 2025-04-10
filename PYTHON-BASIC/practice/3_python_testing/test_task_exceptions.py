55bd4f41b51f0612f58177aa5b9f33ef51dae373
HEMANTVG01 <hhemantvg@griddynamics.com>
2025-04-11 00:30:39 +0530

    Update test_task_exceptions.py

--- PYTHON-BASIC/practice/3_python_testing/test_task_exceptions.py
+++ PYTHON-BASIC/practice/3_python_testing/test_task_exceptions.py
@@ -19,3 +19,28 @@ def test_division_by_zero(capfd):
 
 def test_division_by_one(capfd):
     ...
+
+
+### Solution ###
+import pytest
+
+def division_error(x, y):
+    if y == 0:
+        print("Division by 0")
+    elif y == 1:
+        raise Exception("Deletion on 1 gets the same result")
+    else:
+        return x / y, "Division Finished"
+
+def test_division_by_zero():
+    result = division_error(2, 0)
+    assert result is None
+
+def test_division_by_one():
+    with pytest.raises(Exception) as exc_info:
+        division_error(2, 1)
+    assert str(exc_info.value) == "Deletion on 1 gets the same result"
+
+def test_valid_division():
+    result = division_error(2, 2)
+    assert result == (1.0, "Division Finished")
